124d208f523ac8275bce9c7f63be2fc2c4db76b8
Deepak <deepak.onlinework13@gmail.com>
2022-12-10 14:25:02 +0530

    Search bar and single delete fucntionality working

--- src/Page/Admin.jsx
+++ src/Page/Admin.jsx
@@ -1,4 +1,4 @@
-import { Box, Icon } from "@chakra-ui/react";
+import { Box, Icon, IconButton } from "@chakra-ui/react";
 import React, { useState } from "react";
 import {
   Table,
@@ -11,47 +11,112 @@ import {
   TableCaption,
   TableContainer,
 } from "@chakra-ui/react";
-import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+import { Checkbox, CheckboxGroup } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { EditIcon, DeleteIcon } from '@chakra-ui/icons'
-import { Input } from '@chakra-ui/react';
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import { Input } from "@chakra-ui/react";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
 
 const Admin = () => {
+  const [users, setUsers] = useState([]);
+  const [filterUsers, setFilterUsers] = useState([]);
+  const [filterBy, setFilterBy] = useState("");
 
-    const [users, setUsers] = useState([]);
+  const getUser = () => {
+    fetch(
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res);
+        setUsers(res);
+        setFilterUsers(res);
+      });
+  };
 
-    const getUser = () => {
-        fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
-            .then((res) => res.json())
-            .then((res) => {
-                // console.log(res);
-                setUsers(res)
-            })
+  const handleByFilter = (e) => {
+    if (e.target.value == "") {
+      setUsers(filterUsers);
+    } else {
+      const filterByRes = users.filter(
+        (user) =>
+          user.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+          user.email.toLowerCase().includes(e.target.value.toLowerCase()) ||
+          user.role.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      if (filterByRes.length > 0) {
+        setUsers(filterByRes);
+      } else {
+        setUsers([{"name": "Data not found!"}]);
+      }
     }
+    setFilterBy(e.target.value);
+  };
 
-    useEffect( () => {
-        getUser()
-    }, [])
+  const handleSingleDelete = (id) => {
+    const remainingUsers = users.filter( user => user.id !== id);
+    setUsers(remainingUsers);
+  }
+
+  const handleSingleEdit = (id) => {
+    fetch(`https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json/${id}`,{
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:{
+
+        }
+
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            // console.log(res);
+            setUsers(res);
+            setFilterUsers(res);
+        });
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <Box w='90%' margin='auto'>
+    <Box w="90%" margin="auto">
       <h1
         style={{
           textAlign: "center",
           fontWeight: "700",
           fontSize: "32px",
           color: "grey",
-          marginBottom: "0.6rem"
+          marginBottom: "0.6rem",
         }}
       >
         Admin UI
       </h1>
-      <Input mb='0.6rem' placeholder='Search by name, email or role' />
+      <Input
+        type="search"
+        mb="0.6rem"
+        placeholder="Search by name, email or role"
+        value={filterBy}
+        onInput={(e) => handleByFilter(e)}
+      />
       <TableContainer>
         <Table variant="simple">
           {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
           <Thead>
             <Tr>
-              <Th><Checkbox></Checkbox></Th>
+              <Th>
+                <Checkbox></Checkbox>
+              </Th>
               <Th>Name</Th>
               <Th>Email</Th>
               <Th>Role</Th>
@@ -59,27 +124,37 @@ const Admin = () => {
             </Tr>
           </Thead>
           <Tbody>
-            {
-                users?.map( (user) => (
-                  <Tr>
-                    <Td><Checkbox></Checkbox></Td>
-                    <Td>{user.name}</Td>
-                    <Td>{user.email}</Td>
-                    <Td>{user.role}</Td>
-                    <Td>
-                        <Icon mr='0.8rem' as={EditIcon} w={4} h={4} color='grey.200' />
-                        <Icon as={DeleteIcon} w={4} h={4} color='red.400' />
-                    </Td>
-                  </Tr>
-                ))
-            }
-            
-            
+            {users?.map((user) => (
+              <Tr>
+                <Td>
+                  <Checkbox></Checkbox>
+                </Td>
+                <Td>{user.name}</Td>
+                <Td>{user.email}</Td>
+                <Td>{user.role}</Td>
+                <Td>
+                  <IconButton
+                    icon={<EditIcon/>}
+                    w={4}
+                    h={4}
+                    bgColor="white"
+                    color="grey.200"
+                    onClick={() => handleSingleEdit(user.id)}
+                  />
+                  <IconButton 
+                    icon={<DeleteIcon/>} 
+                    w={4} 
+                    h={4} 
+                    bgColor="white" 
+                    color="red.400" 
+                    onClick={() => handleSingleDelete(user.id)}
+                   />
+                </Td>
+              </Tr>
+            ))}
           </Tbody>
           <Tfoot>
-            <Tr>
-              
-            </Tr>
+            <Tr></Tr>
           </Tfoot>
         </Table>
       </TableContainer>
